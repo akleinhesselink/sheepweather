5e80ebb54a319b7f10518b2e4568c406cb4988f9
andy <andy.kleinhesselink@gmail.com>
2019-04-11 08:59:59 -0700

    Split out rainfall script from merge data script
    Process rainfall data first assigning prcp events to each rainfall
    
    Then merge decagon data with the rainfall data for analysis

---
+++ make_rainfall.R
@@ -0,0 +1,54 @@
+rm(list = ls()) 
+
+library( tidyverse ) 
+library(zoo) # for rollapply 
+
+# input ---------------------------------------------------- # 
+
+load('data/weather.rda')
+
+# originally this used the "USSES_climate.csv" file but I don't know 
+# where this is from and it is less complete than the weather.rda file 
+
+# output ---------------------------------------------------- # 
+
+rainfall_outfile <- 'processed_data/daily_station_dat_rainfall.RDS'
+
+# ---------------------------------------------------------------------------------------
+
+weather$date <- as.POSIXct( strptime( weather$date, '%Y-%m-%d', tz = 'MST') )
+
+weather <- 
+  weather %>% 
+  spread( ELEMENT, value)
+
+weather <- 
+  weather %>% 
+  mutate( TMEAN = ( TMAX + TMIN ) / 2 ) %>% 
+  select(date, PRCP, TMEAN) %>% 
+  mutate( rainfall = rollapply(PRCP, 2, sum, fill = 0, na.rm = TRUE, align = 'right') ) %>%
+  mutate( rainfall = ifelse( rainfall > 0.0 & TMEAN > 3 & !is.na(rainfall), 'rainy', 'not rainy')) %>%
+  mutate( rainfall = ifelse( is.na(rainfall), 'not rainy', rainfall))
+
+# create a factor listing each rainy period, including the day before the rain  
+
+weather <- 
+  weather %>% 
+  arrange( desc(date) ) %>% 
+  mutate( prerain = lag( rainfall, 1) ) %>%
+  mutate( prerain = ifelse( prerain == 'rainy' & rainfall == 'not rainy', TRUE, FALSE)) %>%
+  arrange( date) %>% 
+  mutate( prcp_event = factor( cumsum ( prerain ) )) %>% 
+  group_by( prcp_event, prerain) %>% 
+  mutate( total_rain = cumsum(PRCP) )
+
+weather <- 
+  weather %>% 
+  ungroup() %>% 
+  mutate( simple_date = as.Date( date, tz = 'MST')) %>% 
+  mutate( year = strftime( simple_date, '%Y', tz = 'MST')) %>%  
+  group_by( year ) %>% 
+  arrange( year, simple_date ) %>% 
+  mutate( ann_cum_PRCP = cumsum(PRCP))
+
+saveRDS(weather, rainfall_outfile )
